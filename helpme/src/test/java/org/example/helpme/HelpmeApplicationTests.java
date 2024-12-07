41e1aa774292e094a6109952532784eb75627e77
Mohamed Adel <mohamedadel96e@gmail.com>
2024-12-07 13:57:48 +0200

    Initialize Project Structure

---
+++ helpme/src/test/java/org/example/helpme/HelpmeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.helpme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
