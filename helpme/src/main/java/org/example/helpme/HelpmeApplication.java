cb1bbe1f10eb000eec47e146965dce22c11a09ea
mazen <mazenmashal@gmail.com>
2024-12-07 14:18:40 +0200

    testing branch

--- helpme/src/main/java/org/example/helpme/HelpmeApplication.java
+++ helpme/src/main/java/org/example/helpme/HelpmeApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class HelpmeApplication {
-
+	// hello applicaton tutorial
 	public static void main(String[] args) {
 		SpringApplication.run(HelpmeApplication.class, args);
 	}
